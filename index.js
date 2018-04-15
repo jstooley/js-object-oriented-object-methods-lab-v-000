8ef422ec2defd1c49c1258c0a06278c478f74434
Justin Tooley <jstooley823@gmail.com>
2018-04-15 16:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function BoardMember (name, homeState, training) {
     this.name = name
-    this.homestate = homeState
+    this.homeState = homeState
     this.training = training
     this.veto = function () {
       console.log("No, I must disagree")
     }
     this.approve = function () {
+      console.log("You can do that!")
+    }
+    this.doCharity = function () {
       console.log("You can do that!")
     }
 }
